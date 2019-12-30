ce4511d936bce68fa4efb13cb0e6fae8e18dc0b6
Dusan Radovanovic <dushanr@gmail.com>
2019-12-30 14:50:27 -0800

    Added Bezier curves

--- src/SvgShortestPath.tsx
+++ src/SvgShortestPath.tsx
@@ -10,17 +10,40 @@ interface SvgShortestPathProps {
   end: Position | undefined;
 }
 
-export const SvgShortestPath: React.FC<SvgShortestPathProps> = ({ tableSize, blockMap, fieldSize, lineWidth, start, end }) => {
-  const shortestPath = useMemo(() => calcShortestPath(tableSize, blockMap, start, end), [tableSize, blockMap, start, end]);
+function lerp(p1: Position, p2: Position, f: number): Position {
+  return {
+    x: (1 - f) * p1.x + f * p2.x,
+    y: (1 - f) * p1.y + f * p2.y
+  };
+}
 
-  if (shortestPath) {
-    const svgShortestPath = shortestPath.map((p, i) => `
-      ${i === 0 ? 'M' : 'L'}
-      ${p.x * fieldSize + fieldSize / 2 + lineWidth / 2} ${p.y * fieldSize + fieldSize / 2 + lineWidth / 2}
-    `).join(' ');
+export const SvgShortestPath: React.FC<SvgShortestPathProps> = ({ tableSize, blockMap, fieldSize, lineWidth, start, end }) => {
+  const p = useMemo(() => calcShortestPath(tableSize, blockMap, start, end), [tableSize, blockMap, start, end]);
+  const scrOffset = fieldSize / 2 + lineWidth / 2;
+
+  const scr = (q: Position) => {
+    return `${q.x * fieldSize + scrOffset} ${q.y * fieldSize + scrOffset}`;
+  }
+
+  if (p) {
+    const d = [`M ${scr(p[0])}`];
+
+    for (let i = 1; i < p.length - 1; i++) {
+      const A = lerp(p[i - 1], p[i], 0.8);
+      const B = lerp(p[i], p[i + 1], 0.2);
+      d.push(`L ${scr(A)}`);
+      d.push(`Q ${scr(p[i])} ${scr(B)}`);
+    }
+
+    d.push(`L ${scr(p[p.length - 1])}`);
+
+    // const d = p.map((p, i) => `
+    //   ${i === 0 ? 'M' : 'L'}
+    //   ${p.x * fieldSize + fieldSize / 2 + lineWidth / 2} ${p.y * fieldSize + fieldSize / 2 + lineWidth / 2}
+    // `);
 
     return (
-      <path d={svgShortestPath} fill="none" stroke="#000" />
+      <path d={d.join(' ')} fill="none" stroke="#000" />
     );
   } else {
     return (<></>);
